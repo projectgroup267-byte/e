52bdb9adb95db8350f9b406bc7ff5bb5eaf75389
projectgroup267-byte <projectgroup267@gmail.com>
2025-12-18 12:25:13 +0530

    Update e.py

--- e.py
+++ e.py
@@ -19,7 +19,7 @@ st.title("📊 Engagement Rate Analysis")
 # ----------------------------
 @st.cache_data
 def load_data():
-    df = pd.read_csv("social_media_engagement_enhanced (1)")
+    df = pd.read_csv("social_media_engagement_enhanced (1).csv")
     return df
 
 df = load_data()
